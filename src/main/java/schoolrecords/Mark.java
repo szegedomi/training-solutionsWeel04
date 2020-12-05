16fa1a836396eb2d50dd824271917640a6c98cbc
szegedomi <31705962+szegedomi@users.noreply.github.com>
2020-12-05 16:01:40 +0100

    preliminary push, partly solved project tast

---
+++ src/main/java/schoolrecords/Mark.java
@@ -0,0 +1,49 @@
+package schoolrecords;
+
+public class Mark {
+
+    private MarkType markType;
+    private Subject subject;
+    private Tutor tutor;
+
+    public MarkType getMarkType() {
+        return markType;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public Mark(MarkType markType, Subject subject, Tutor tutor){
+        this.markType = markType;
+        if (isEmpty(subject.getSubjectName())){
+            throw new NullPointerException("Both subject and tutor must be provided!");
+        }
+        this.subject = subject;
+        if (isEmpty(tutor.getName())){
+            throw new NullPointerException("Both subject and tutor must be provided!");
+        }
+        this.tutor = tutor;
+    }
+
+    public Mark(String markType, Subject subject, Tutor tutor){
+        this.markType = MarkType.valueOf(markType);
+        if (subject == null){
+            throw new NullPointerException("Both subject and tutor must be provided!");
+        }
+        this.subject = subject;
+        if (tutor == null){
+            throw new NullPointerException("Both subject and tutor must be provided!");
+        }
+        this.tutor = tutor;
+    }
+
+    public boolean isEmpty(String empty){
+        return empty == null || "".equals(empty);
+    }
+
+}
---
+++ src/main/java/schoolrecords/Tutor.java
@@ -0,0 +1,29 @@
+package schoolrecords;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tutor {
+
+    private String name;
+    private List<Subject> subjects = new ArrayList<>();
+
+    public Tutor(String name, List<Subject> subjects) {
+        this.name = name;
+        this.subjects = subjects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean tutorTeachingSubject(Subject subject){
+        for (Subject subject1 : this.subjects){
+            if(subject1.getSubjectName().equals(subject.getSubjectName())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
